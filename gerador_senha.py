48c3f3259bb98de2f1e267e35d9cfa53d4d728f1
osouza <mauricio.souza002@gmail.com>
2022-12-06 17:47:38 -0200

    Criar funções gerador_senha e continuar_sair

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,23 +1,30 @@
 import secrets
 import string
-import time
 
-letras = string.ascii_letters
-digitos = string.digits
-caracteres_especiais = string.punctuation
+def continuar_sair():
+    resposta = input("Criar nova senha? S/N.\n").lower()
+    if resposta == "s":
+        gerador_senha()
+    elif resposta == "n":
+        print("Até logo!")
+        quit()
+    else:
+        print("Input inválido. Tente novamente.")
+        continuar_sair()
 
-alfabeto = letras + digitos + caracteres_especiais
+def gerador_senha():
+    letras = string.ascii_letters
+    digitos = string.digits
+    caracteres_especiais = string.punctuation
+    alfabeto = letras + digitos + caracteres_especiais
+    comprimento_senha = input("Comprimento da(s) senha(s) (mínimo: 8 caracteres):\n")
+    senha = ""
+    for contagem in range(int(comprimento_senha)):
+        senha += "".join(secrets.choice(alfabeto))
+        if (any(caractere in caracteres_especiais for caractere in senha) and
+            sum(caractere in digitos for caractere in senha)>=8):
+            break
+    print(f"Sua senha é '{senha}'.\n")
+    continuar_sair()
 
-# quantidade_senha = input("Quantidades de senha que deseja criar:\n")
-comprimento_senha = input("Comprimento da senha (mínimo):\n")
-
-senha = ""
-
-for contagem in range(int(comprimento_senha)):
-    senha += "".join(secrets.choice(alfabeto))
-
-if (any(caractere in caracteres_especiais for caractere in senha) and
-    sum(caractere in digitos for caractere in senha)>=6):
-        break
-
-print(senha)
+gerador_senha()
