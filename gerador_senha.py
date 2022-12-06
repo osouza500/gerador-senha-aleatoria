8d28da3cbc321615cd0c250848f0dba627bd55f9
osouza <mauricio.souza002@gmail.com>
2022-12-06 14:30:24 -0200

    Inserir instrução if com funções any e sum

--- gerador_senha.py
+++ gerador_senha.py
@@ -1,5 +1,6 @@
 import secrets
-import string 
+import string
+import time
 
 letras = string.ascii_letters
 digitos = string.digits
@@ -8,11 +9,15 @@ caracteres_especiais = string.punctuation
 alfabeto = letras + digitos + caracteres_especiais
 
 # quantidade_senha = input("Quantidades de senha que deseja criar:\n")
-comprimento_senha = input("Comprimento da senha (em caracteres):\n")
+comprimento_senha = input("Comprimento da senha (mínimo):\n")
 
 senha = ""
 
 for contagem in range(int(comprimento_senha)):
     senha += "".join(secrets.choice(alfabeto))
+
+if (any(caractere in caracteres_especiais for caractere in senha) and
+    sum(caractere in digitos for caractere in senha)>=6):
+        break
 
 print(senha)
