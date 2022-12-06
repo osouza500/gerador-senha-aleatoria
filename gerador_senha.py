725c85cce8fe17c99dbe52502c2edab2ecfe1ca7
osouza <mauricio.souza002@gmail.com>
2022-12-06 14:00:49 -0200

    Importar módulos secrets e string; declarar constantes p/ caracteres

---
+++ gerador_senha.py
@@ -0,0 +1,10 @@
+import secrets
+import string 
+
+letras = string.ascii_letters
+digitos = string.digits
+caracteres_especiais = string.punctuation
+
+alfabeto = letras + digitos + caracteres_especiais
+
+print(alfabeto)
