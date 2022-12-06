315b68d50cc7b2525b2aa9fd44ab22872b53cc7c
osouza <mauricio.souza002@gmail.com>
2022-12-06 14:09:53 -0200

    Corrigir loop for c/ métodos range e int

--- gerador_senha.py
+++ gerador_senha.py
@@ -7,12 +7,12 @@ caracteres_especiais = string.punctuation
 
 alfabeto = letras + digitos + caracteres_especiais
 
-quantidade_senha = input("Quantidades de senha que deseja criar:\n")
+# quantidade_senha = input("Quantidades de senha que deseja criar:\n")
 comprimento_senha = input("Comprimento da senha (em caracteres):\n")
 
 senha = ""
 
-for contagem in comprimento_senha:
+for contagem in range(int(comprimento_senha)):
     senha += "".join(secrets.choice(alfabeto))
 
 print(senha)
